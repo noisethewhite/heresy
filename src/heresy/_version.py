ae9e165a57339c3a905647feffe09f2f90c3c5a6
Noise The White <email@example.com>
2026-03-07 21:23:30 +0100

    heh

--- src/heresy/_version.py
+++ src/heresy/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.7.post1.dev0'
-__version_tuple__ = version_tuple = (0, 1, 7, 'post1', 'dev0')
+__version__ = version = '0.1.8.post1.dev0'
+__version_tuple__ = version_tuple = (0, 1, 8, 'post1', 'dev0')
 
-__commit_id__ = commit_id = 'g941dbd099'
+__commit_id__ = commit_id = 'ga7bc48f3c'
