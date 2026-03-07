165f3bae35e079c303fee77a9e0e95ea5339eb7f
Noise The White <email@example.com>
2026-03-07 20:39:14 +0100

    ...

--- src/heresy/__init__.py
+++ src/heresy/__init__.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from ._version import __version__
 
-__all__ = ["__version__"]
+__all__: list[str] = ["__version__"]
--- src/heresy/_version.py
+++ src/heresy/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.4.post1.dev1'
-__version_tuple__ = version_tuple = (0, 1, 4, 'post1', 'dev1')
+__version__ = version = '0.1.5.post1.dev1'
+__version_tuple__ = version_tuple = (0, 1, 5, 'post1', 'dev1')
 
-__commit_id__ = commit_id = 'ge25655ee6'
+__commit_id__ = commit_id = 'g3d9721760'
