2e9295af170de2c147bd254a7c396612672f52aa
Noise The White <email@example.com>
2026-03-09 08:25:46 +0100

    forgit to add imports to __init__

--- src/heresy/__init__.py
+++ src/heresy/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from ._version import __version__
-from . import singletons
+from ._root_submodule.singleton.singleton import singleton
+from ._root_submodule.singleton.singletonproperty import singletonproperty
+from ._root_submodule.singleton.singletonmethod import singletonmethod
 
-
-__all__: list[str] = ["__version__", "singletons"]
+__all__: list[str] = ["__version__", "singleton", "singletonproperty", "singletonmethod"]
