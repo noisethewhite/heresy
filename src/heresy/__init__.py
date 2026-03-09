4cca52487564e1895348afca773b5b4563f7ddff
Noise The White <email@example.com>
2026-03-09 08:22:03 +0100

    added singletons

--- src/heresy/__init__.py
+++ src/heresy/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from ._version import __version__
+from . import singletons
 
 
-__all__: list[str] = ["__version__"]
+__all__: list[str] = ["__version__", "singletons"]
